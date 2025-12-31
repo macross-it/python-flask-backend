eeecc63ca658acd5e2a538168340c092725aadb2
macross-it <51770979+macross-it@users.noreply.github.com>
2025-12-30 19:19:37 -0500

    Fix Amplify 404 issue2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return jsonify(message="Hello World from Flask!")
+    return {"status": "Backend running on Amplify"}
 
 @app.route("/health")
 def health():
